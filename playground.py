47e73e06c52c6c74f117109e67c05e83d1222870
aminyous <amyn.hichri@gmail.com>
2023-11-20 21:01:14 +0100

    isdigit isupper

--- playground.py
+++ playground.py
@@ -1,7 +1,36 @@
-test = "edit 2est dfdsf"
+# 8 or more
+# at least one digit
+# at least one capital
+# Strong password
 
-new = int(test[5:6])
-print(type(new))
+answer = "Y"
+check_digit = False
+check_upper = False
+
+while answer == "Y":
+
+    password = input("Enter new password: ")
+
+    for item in password:
+        if item.isdigit():
+            check_digit = True
+            continue
+
+        if item.isupper():
+            check_upper = True
+
+    if len(password) >= 8 and check_digit and check_upper:
+        print("Strong Password")
+    else:
+        print("Weak Password!")
+
+    answer = input("Do you want to retry ? Y/N").capitalize()
+    check_digit = False
+    check_upper = False
+
+print("*** BYE ***")
+
+
 
 
 
