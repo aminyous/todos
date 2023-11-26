f8144771afacdd21ab844e0c5f95ab8751800182
aminyous <amyn.hichri@gmail.com>
2023-11-26 21:12:46 +0100

    apply sum() to a list of floats in a costum function

--- playground.py
+++ playground.py
@@ -1,2 +1,13 @@
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-print(letters[4:])
+def get_from_file():
+    with open("file/data.txt", "r") as file:
+        return file.readlines()
+
+
+def avg_int():
+    temperatures = get_from_file()
+    temperatures_cleaned = [float(item) for item in temperatures[1:]]
+    return sum(temperatures_cleaned) / len(temperatures_cleaned)
+
+
+print(f"Average temperature is : {avg_int()}")
+
