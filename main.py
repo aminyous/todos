772b0f15912886f7a5027fb745e334b7b4c1b636
aminyous <amyn.hichri@gmail.com>
2023-11-28 22:06:11 +0100

    Add a doc-string to two functions

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ path = "todos.txt"
 
 
 def get_todo(filepath=path):
+    """ Read a text file and return the content in a form of a list """
+
     with open(filepath, "r") as file:
         todos_file = file.readlines()
     return todos_file
 
 
 def set_todo(data, filepath=path):
+    """ Write a list in the text file """
+
     with open(filepath, "w") as file:
         file.writelines(data)
 
