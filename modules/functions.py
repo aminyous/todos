0b615e94efe33fe51476329c42c64d96d8c43c4b
aminyous <amyn.hichri@gmail.com>
2023-11-30 21:44:33 +0100

    Create module to put the functions seperately, readable code

---
+++ modules/functions.py
@@ -0,0 +1,21 @@
+path = "todos.txt"
+
+
+def get_todo(filepath=path):
+    """ Read a text file and return the content in a form of a list """
+
+    with open(filepath, "r") as file:
+        todos_file = file.readlines()
+    return todos_file
+
+
+def set_todo(data, filepath=path):
+    """ Write a list in the text file """
+
+    with open(filepath, "w") as file:
+        file.writelines(data)
+
+
+def get_user_answer():
+    user_answer = input("Type add, show, edit, complete or exit:")
+    return user_answer.strip()
