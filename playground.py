2c1ff034c148d3e5318ac808b4f9018455103b9a
aminyous <amyn.hichri@gmail.com>
2023-12-03 22:30:12 +0100

    Using the random module

--- playground.py
+++ playground.py
@@ -1,29 +1,6 @@
-import json
+import random
 
-with open("file/questions.json", "r") as file:
-    content = file.read()
+low_number = int(input("Enter the lower bound: "))
+upper_number = int(input("Enter the upper bound: "))
 
-data = json.loads(content)
-
-answers = []
-score = 0
-
-for item in data:
-    alternatives = [f"{i + 1} - {el}" for i, el in enumerate(item["Alternatives"])]
-    alternatives = ", ".join(alternatives)
-
-    user_answer = int(input(f"{item['Question_text']} : {alternatives} :"))
-
-    item["user answer"] = user_answer
-
-
-for index, item in enumerate(data):
-    if item["Answer"] == item["user answer"]:
-        score = score + 1
-        print(f"{index + 1} - Correct answer :) User answer: {item['user answer']}, Correct Answer : {item['Answer']}")
-    else:
-        print(f"{index + 1} - Incorrect answer :( User answer: {item['user answer']}, Correct Answer : {item['Answer']}")
-
-print(f"Score = {score} / {len(data)}")
-
-
+print(random.randint(low_number, upper_number))
