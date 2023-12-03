7f9749ae3a4bf3e248fcf4cfa080e8dc2eb29d83
aminyous <amyn.hichri@gmail.com>
2023-12-03 20:19:11 +0100

    Using Json and manipulate it (parse and using data located inside  Json file)

--- playground.py
+++ playground.py
@@ -1,8 +1,29 @@
-import webbrowser
+import json
 
-user_term = input("Enter your search term: ")
+with open("file/questions.json", "r") as file:
+    content = file.read()
 
-webbrowser.open(f"https://www.google.fr/maps/place/{user_term}")
+data = json.loads(content)
 
+answers = []
+score = 0
+
+for item in data:
+    alternatives = [f"{i + 1} - {el}" for i, el in enumerate(item["Alternatives"])]
+    alternatives = ", ".join(alternatives)
+
+    user_answer = int(input(f"{item['Question_text']} : {alternatives} :"))
+
+    item["user answer"] = user_answer
+
+
+for index, item in enumerate(data):
+    if item["Answer"] == item["user answer"]:
+        score = score + 1
+        print(f"{index + 1} - Correct answer :) User answer: {item['user answer']}, Correct Answer : {item['Answer']}")
+    else:
+        print(f"{index + 1} - Incorrect answer :( User answer: {item['user answer']}, Correct Answer : {item['Answer']}")
+
+print(f"Score = {score} / {len(data)}")
 
 
